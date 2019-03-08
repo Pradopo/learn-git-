f610a392cdf1ef7a2efcc070f3b8b50aab800910
Pradopo <Pradopo@CENTURION>
2019-03-08 20:18:20 +0800

    commit all

--- castlewindsor/Program.cs
+++ castlewindsor/Program.cs
@@ -13,7 +13,19 @@ namespace castlewindsor
 
             Console.WriteLine("------start-------");
             Console.WriteLine("------------------");
-            Console.WriteLine(string.Format("Name::{0}","Parang Pradopo"));
+            Console.WriteLine("Initiate object");
+            var user = new User()
+            {
+                Name = "Parang Pradopo",
+                ID = "001",
+                Address= "Pasir Ris",
+                Email = "pradopo@gmail.com"
+            };
+            Console.WriteLine(string.Format("ID::{0}",user.ID));
+            Console.WriteLine(string.Format("Name::{0}", user.Name));
+            Console.WriteLine(string.Format("Address::{0}", user.Address));
+
+            Console.WriteLine("---finish---");
             Console.ReadKey();
         }
     }
