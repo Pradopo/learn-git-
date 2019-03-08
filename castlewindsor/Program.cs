89ccbca5b541681d0092e80789e021a03cd11322
Pradopo <Pradopo@CENTURION>
2019-03-08 22:14:18 +0800

    change logic

--- castlewindsor/Program.cs
+++ castlewindsor/Program.cs
@@ -13,16 +13,27 @@ namespace castlewindsor
 
             Console.WriteLine("------start-------");
             Console.WriteLine("------------------");
+
             Console.WriteLine("Initiate object");
             var user = new User()
             {
                 Name = "Parang Pradopo",
                 ID = "001",
-                Address= "Pasir Ris",
+
+                Address = "Pasir Ris",
                 Email = "pradopo@gmail.com",
+
+
+
                 Status = "Active"
             };
             Console.WriteLine(string.Format("ID::{0}",user.ID));
+
+            for (var i = 1; i <=3; i++ ) {
+
+                Console.WriteLine(string.Format("Name loop-{1}::{0}", user.Name,i));
+                Console.WriteLine(string.Format("Address loop-{1}::{0}", user.Address,i));
+            }
             Console.WriteLine(string.Format("Name::{0}", user.Name));
             Console.WriteLine(string.Format("Address::{0}", user.Address));
             Console.WriteLine(string.Format("Status::{0}", user.Status));
