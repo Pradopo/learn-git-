52d4e12b48e530ef69120ec866f023fb330ba1a4
Pradopo <Pradopo@CENTURION>
2019-03-09 10:44:08 +0800

    revert data

--- castlewindsor/Program.cs
+++ castlewindsor/Program.cs
@@ -8,40 +8,24 @@ namespace castlewindsor
 {
     class Program
     {
-        /// <summary>
-        /// main function 
-        /// </summary>
-        /// <param name="args">array pf argument for program</param>
         static void Main(string[] args)
         {
 
             Console.WriteLine("------start-------");
             Console.WriteLine("------------------");
-
             Console.WriteLine("Initiate object");
             var user = new User()
             {
                 Name = "Parang Pradopo",
                 ID = "001",
-
-                Address = "Pasir Ris",
-                Email = "pradopo@gmail.com",
-
-
-
-                Status = "Active"
+                Address= "Pasir Ris",
+                Email = "pradopo@gmail.com"
             };
             Console.WriteLine(string.Format("ID::{0}",user.ID));
-
-            for (var i = 1; i <=4; i++ ) {
-
-                Console.WriteLine(string.Format("Name loop-{1}::{0}", user.Name,i));
-                Console.WriteLine(string.Format("Address loop-{1}::{0}", user.Address,i));
-            }
             Console.WriteLine(string.Format("Name::{0}", user.Name));
             Console.WriteLine(string.Format("Address::{0}", user.Address));
-            Console.WriteLine(string.Format("Status::{0}", user.Status));
-            Console.WriteLine("---finish data---");
+
+            Console.WriteLine("---finish---");
             Console.ReadKey();
         }
     }
